717c8deae18a2d32e6717e48578cbc1ce1a04805
Rob Dickens <rob.dickens@lafros.com>
2012-05-24 12:33:59 +0100

    qed; "foreach", "map" scalatest tests added: former only prints b since type is Any - why?
    .gitignore
    +target/

---
+++ src/test/scala/Tests.scala
@@ -0,0 +1,29 @@
+import org.scalatest.{FunSuite, matchers}
+import matchers.ShouldMatchers
+import scala.{Either => _, Left => _, Right => _}
+
+class Tests extends FunSuite with ShouldMatchers {
+  val intVal = 1
+  val either: Either[String, Int] = Right(intVal)
+
+  test("foreach") {
+    val intVal2 = 1
+    var res = 0
+    for {
+      a <- either.right
+      b = a + intVal2
+    } println(b)
+    //} res = b
+    //res should equal(intVal + intVal2)
+  }
+
+  test("map") {
+    val intVal2 = 1
+    val res = for {
+      a <- either.right
+      b = a + intVal2
+    } yield b
+    res should equal(Either.RightProjection(Right(intVal + intVal2)))
+  }
+}
+
