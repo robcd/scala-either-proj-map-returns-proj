0906cd856cdddd3bf227f0e6fa3170f4f485b276
Rob Dickens <rob.dickens@lafros.com>
2012-05-24 15:10:59 +0100

    Tests: "foreach", "map" tests now have left and right version

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -4,10 +4,20 @@ import scala.{Either => _, Left => _, Right => _}
 
 class Tests extends FunSuite with ShouldMatchers {
   val intVal = 1
-  val either: Either[String, Int] = Right(intVal)
+  val intVal2 = 1
 
-  test("foreach") {
-    val intVal2 = 1
+  test("left foreach") {
+    val either: Either[Int, String] = Left(intVal)
+    var res = 0
+    for {
+      a <- either.left
+      b = a + intVal2
+    } res = b
+    res should equal(intVal + intVal2)
+  }
+
+  test("right foreach") {
+    val either: Either[String, Int] = Right(intVal)
     var res = 0
     for {
       a <- either.right
@@ -16,8 +26,17 @@ class Tests extends FunSuite with ShouldMatchers {
     res should equal(intVal + intVal2)
   }
 
-  test("map") {
-    val intVal2 = 1
+  test("left map") {
+    val either: Either[Int, String] = Left(intVal)
+    val res = for {
+      a <- either.left
+      b = a + intVal2
+    } yield b
+    res should equal(Either.LeftProjection(Left(intVal + intVal2)))
+  }
+
+  test("right map") {
+    val either: Either[String, Int] = Right(intVal)
     val res = for {
       a <- either.right
       b = a + intVal2
