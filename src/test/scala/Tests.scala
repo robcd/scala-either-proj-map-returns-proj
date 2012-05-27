a9a6d2660e91a356eb21e556e0f74abc9ffe81ad
Rob Dickens <rob.dickens@lafros.com>
2012-05-27 14:31:20 +0100

    Tests: intVal, intVal2 => 1 (makes things a bit clearer)

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -3,27 +3,24 @@ import matchers.ShouldMatchers
 import scala.{Either => _, Left => _, Right => _}
 
 class Tests extends FunSuite with ShouldMatchers {
-  val intVal = 1
-  val intVal2 = 1
-
   test("left foreach true") {
-    val either: Either[Int, String] = Left(intVal)
+    val either: Either[Int, String] = Left(1)
     var res = 0
     for {
       a <- either.left
-      b = a + intVal2
+      b = a + 1
       if b > 0
     } res = b
 
-    res should equal(intVal + intVal2)
+    res should equal(2)
   }
 
   test("left foreach false") {
-    val either: Either[Int, String] = Left(intVal)
+    val either: Either[Int, String] = Left(1)
     var res = 0
     for {
       a <- either.left
-      b = a + intVal2
+      b = a + 1
       if b < 0
     } res = b
 
@@ -31,101 +28,101 @@ class Tests extends FunSuite with ShouldMatchers {
   }
 
   test("right foreach true") {
-    val either: Either[String, Int] = Right(intVal)
+    val either: Either[String, Int] = Right(1)
     var res = 0
     for {
       a <- either.right
-      b = a + intVal2
+      b = a + 1
       if b > 0
     } res = b
-    res should equal(intVal + intVal2)
+    res should equal(2)
   }
 
   test("right foreach false") {
-    val either: Either[String, Int] = Right(intVal)
+    val either: Either[String, Int] = Right(1)
     var res = 0
     for {
       a <- either.right
-      b = a + intVal2
+      b = a + 1
       if b < 0
     } res = b
     res should equal(0)
   }
 
   test("left map true") {
-    val either: Either[Int, String] = Left(intVal)
+    val either: Either[Int, String] = Left(1)
     val left = for {
       a <- either.left
-      b = a + intVal2
+      b = a + 1
       if b > 0
     } yield b
-    left.e should equal(Left(intVal + intVal2))
-    left.get should equal(intVal + intVal2)
-    left.getOrElse(0) should equal(intVal + intVal2)
-    left.forall(_ == intVal) should be(false)
-    left.forall(_ == intVal + intVal2) should be(true)
-    left.exists(_ == intVal) should be(false)
-    left.exists(_ == intVal + intVal2) should be(true)
-    left.toSeq should equal(Seq(intVal + intVal2))
-    left.toOption should equal(Some(intVal + intVal2))
+    left.e should equal(Left(2))
+    left.get should equal(2)
+    left.getOrElse(0) should equal(2)
+    left.forall(_ == 1) should be(false)
+    left.forall(_ == 2) should be(true)
+    left.exists(_ == 1) should be(false)
+    left.exists(_ == 2) should be(true)
+    left.toSeq should equal(Seq(2))
+    left.toOption should equal(Some(2))
   }
 
   test("left map false") {
-    val either: Either[Int, String] = Left(intVal)
+    val either: Either[Int, String] = Left(1)
     val left = for {
       a <- either.left
-      b = a + intVal2
+      b = a + 1
       if b < 0
     } yield b
-    left.e should equal(LeftAsRight(Left(intVal + intVal2)))
+    left.e should equal(LeftAsRight(Left(2)))
     val thrown = intercept[NoSuchElementException] {
       left.get
     }
     thrown.getMessage should equal("Either.left.value on Right")
     left.getOrElse(0) should equal(0)
-    left.forall(_ == intVal) should be(true) // since no elements
-    left.forall(_ == intVal + intVal2) should be(true) // "
-    left.exists(_ == intVal) should be(false) // "
-    left.exists(_ == intVal + intVal2) should be(false) // "
+    left.forall(_ == 1) should be(true) // since no elements
+    left.forall(_ == 2) should be(true) // "
+    left.exists(_ == 1) should be(false) // "
+    left.exists(_ == 2) should be(false) // "
     left.toSeq should equal(Seq())
     left.toOption should equal(None)
   }
 
   test("right map true") {
-    val either: Either[String, Int] = Right(intVal)
+    val either: Either[String, Int] = Right(1)
     val right = for {
       a <- either.right
-      b = a + intVal2
+      b = a + 1
       if b > 0
     } yield b
-    right.e should equal(Right(intVal + intVal2))
-    right.get should equal(intVal + intVal2)
-    right.getOrElse(0) should equal(intVal + intVal2)
-    right.forall(_ == intVal) should be(false)
-    right.forall(_ == intVal + intVal2) should be(true)
-    right.exists(_ == intVal) should be(false)
-    right.exists(_ == intVal + intVal2) should be(true)
-    right.toSeq should equal(Seq(intVal + intVal2))
-    right.toOption should equal(Some(intVal + intVal2))
+    right.e should equal(Right(2))
+    right.get should equal(2)
+    right.getOrElse(0) should equal(2)
+    right.forall(_ == 1) should be(false)
+    right.forall(_ == 2) should be(true)
+    right.exists(_ == 1) should be(false)
+    right.exists(_ == 2) should be(true)
+    right.toSeq should equal(Seq(2))
+    right.toOption should equal(Some(2))
   }
 
   test("right map false") {
-    val either: Either[String, Int] = Right(intVal)
+    val either: Either[String, Int] = Right(1)
     val right = for {
       a <- either.right
-      b = a + intVal2
+      b = a + 1
       if b < 0
     } yield b
-    right.e should equal(RightAsLeft(Right(intVal + intVal2)))
+    right.e should equal(RightAsLeft(Right(2)))
     val thrown = intercept[NoSuchElementException] {
       right.get
     }
     thrown.getMessage should equal("Either.right.value on Left")
     right.getOrElse(0) should equal(0)
-    right.forall(_ == intVal) should be(true) // since no elements
-    right.forall(_ == intVal + intVal2) should be(true) // "
-    right.exists(_ == intVal) should be(false) // "
-    right.exists(_ == intVal + intVal2) should be(false) // "
+    right.forall(_ == 1) should be(true) // since no elements
+    right.forall(_ == 2) should be(true) // "
+    right.exists(_ == 1) should be(false) // "
+    right.exists(_ == 2) should be(false) // "
     right.toSeq should equal(Seq())
     right.toOption should equal(None)
   }
@@ -135,28 +132,28 @@ class Tests extends FunSuite with ShouldMatchers {
 
   test("two right generators with foreach - succeeds 1") {
     var res = 0
-    val a = intVal + intVal2
+    val a = 2
     for {
       b <- gt0(a).right
       c <- gt1(b).right
     } res = c
-    res should equal(a)
+    res should equal(2)
   }
 
   test("two right generators with foreach - succeeds 2") {
     var res = 0
-    val a = intVal
+    val a = 1
     for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
     } res = d
-    res should equal(a + intVal2)
+    res should equal(2)
   }
 
   test("two right generators with foreach - fails") {
     var res = 0
-    val a = intVal
+    val a = 1
     for {
       b <- gt0(a).right
       c <- gt1(b).right
@@ -166,22 +163,22 @@ class Tests extends FunSuite with ShouldMatchers {
 
   test("two right generators with foreach true") {
     var res = 0
-    val a = intVal
+    val a = 1
     for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
       if d > 0
     } res = d
-    res should equal(a + intVal2)
+    res should equal(2)
   }
 
   test("two right generators with foreach false") {
     var res = 0
-    val a = intVal
+    val a = 1
     for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
       if d < 0
     } res = d
@@ -189,7 +186,7 @@ class Tests extends FunSuite with ShouldMatchers {
   }
 
   test("two right generators with map - succeeds 1") {
-    val a = intVal + intVal2
+    val a = 2
     val right = for {
       b <- gt0(a).right
       c <- gt1(b).right
@@ -198,17 +195,17 @@ class Tests extends FunSuite with ShouldMatchers {
   }
 
   test("two right generators with map - succeeds 2") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
     } yield d
-    right.e should equal(Right(a + intVal2))
+    right.e should equal(Right(2))
   }
 
   test("two right generators with map - fails") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
       c <- gt1(b).right
@@ -218,47 +215,47 @@ class Tests extends FunSuite with ShouldMatchers {
   }
 
   test("two right generators with map true") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
       if d > 0
     } yield d
-    right.e should equal(Right(a + intVal2))
+    right.e should equal(Right(2))
   }
 
   test("two right generators with map true 2") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       if c > 0
       d <- gt1(c).right
     } yield d
-    right.e should equal(Right(a + intVal2))
+    right.e should equal(Right(2))
   }
 
   test("two right generators with map false") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       d <- gt1(c).right
       if d < 0
     } yield d
-    right.e should equal(RightAsLeft(Right(a + intVal2)))
+    right.e should equal(RightAsLeft(Right(2)))
   }
 
   test("two right generators with map false 2") {
-    val a = intVal
+    val a = 1
     val right = for {
       b <- gt0(a).right
-      c = b + intVal2
+      c = b + 1
       if c < 0
       d <- gt1(c).right
     } yield d
-    right.e should equal(RightAsLeft(Right(a + intVal2)))
+    right.e should equal(RightAsLeft(Right(2)))
   }
 }
 
